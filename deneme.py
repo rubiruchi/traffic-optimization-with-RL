a606e53cd044f1fc5c6b3bb99910a04d5fcad3af
kazimsanlav <kazimsanlav@gmail.com>
2019-03-19 13:44:58 +0300

    initialize

---
+++ deneme.py
@@ -0,0 +1,73 @@
+import make_env_
+# from gym.spaces 
+import prng
+import numpy as np
+import random
+
+# creates an multiagent environment which has reset, render, step
+env = make_env_.make_env('swarm2') #! 2vs1 Swarm environment
+# env = make_env_.make_env('simple_tag_guided_1v2')
+# create interactive policies for each agent
+# policies = [InteractivePolicy(env,i) for i in range(env.n)]
+print(env.observation_space)
+print(env.action_space)
+# env.observation_space
+ 
+
+# exit()
+# print(policies)
+# exit()
+
+
+def sample_actions():
+    action = [env.action_space[i].sample() for i in range(env.n)]
+    action.insert(0,0)
+    # print(action)
+    return action
+
+def deterministic_actions():
+    action = fixed() 
+    action.insert(0,0)
+    # print(action)
+    return action
+def deterministic_actions2():
+    action = fixed2() 
+    action.insert(0,0)
+    # print(action)
+    return action
+
+def fixed():
+	# act =  list(np.random.uniform(-1,0.5,4))
+	act = [random.uniform(-0.1,0.1),0,0,1]
+	return act
+def fixed2():
+	# act =  list(np.random.uniform(-1,0.5,4))
+	act = [0,1,0,random.uniform(-0.1,0.1)]
+	return act
+
+for i_episode in range(2):# number of simulations 
+    observation = env.reset()
+    # print(len(observation[0]))
+    for t in range(100):# number of steps
+        env.render()
+	
+        my_action = [ deterministic_actions()  if i < env.n/2 else deterministic_actions2()for i in range(env.n) ]
+        # my_action = sample_actions()
+        
+        next_state, reward, done, info = env.step(my_action)
+        print('*'*30)
+        print('next_state\n',next_state)
+        print('\nreward\n',reward)
+        print('\ndone\n',done)
+        print('\ninfo\n',info)
+        print('*'*30)
+        # print(len(observation)) 
+        # print(observation[0].shape) 
+        # print(observation[0]) 
+
+env.close()
+
+
+# obs = [np.array([ 0.        ,  0.        , -0.71170829,  0.82181501,  0.62097461, -0.89593826,  1.14709038,  0.16188317,  0.        ,  0.        ,0.        ,  0.        ]),
+#  np.array([ 0.        ,  0.        , -0.09073367, -0.07412326, -0.62097461,  0.89593826,  0.52611577,  1.05782144,  0.        ,  0.        ,0.        ,  0.        ]), 
+#  np.array([ 0.        ,  0.        ,  0.43538209,  0.98369818, -1.14709038, -0.16188317, -0.52611577, -1.05782144,  0.        ,  0.        ,0.        ,  0.        ])]
---
+++ multiagent/scenarios/simple.py
@@ -0,0 +1,81 @@
+import numpy as np
+from multiagent.core import World, Agent, Landmark
+from multiagent.scenario import BaseScenario
+import random 
+class Scenario(BaseScenario):
+    def make_world(self):
+        world = World()
+        num_good_agents = 5
+        num_adversaries = 5
+        num_landmarks = 40
+        num_agents = num_adversaries + num_good_agents
+        world.agents = [Agent() for i in range(num_agents)]
+        # add agents
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = True
+            agent.silent = True
+            agent.adversary = True if i < num_adversaries else False
+            agent.size = 0.075 if agent.adversary else 0.075
+            agent.accel = 3.0 if agent.adversary else 4.0
+            #agent.accel = 20.0 if agent.adversary else 25.0
+            agent.max_speed = 1.0 if agent.adversary else 1.3
+            # agent.max_speed = 1.0/5 if agent.adversary else 1.3/5
+
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = True
+            landmark.movable = False
+            landmark.size = 0.05
+            landmark.boundary = False
+        # make initial conditions
+        self.reset_world(world)
+        return world
+
+    def reset_world(self, world):
+        # random properties for agents
+        for i, agent in enumerate(world.agents):
+            agent.color = np.array([0.35, 0.85, 0.35]) if not agent.adversary else np.array([0.85, 0.35, 0.35])
+        # random properties for landmarks
+        for i, landmark in enumerate(world.landmarks):
+            landmark.color = np.array([0.75,0.75,0.75])
+        # set random initial states
+        for agent in world.agents:
+            # agent.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
+            agent.state.p_pos = [0.5, 0.6]
+            # [0.8+random.uniform(-0.1,0.1), 0.5+random.uniform(-0.1,0.1)] if not agent.adversary else [0.2+random.uniform(-0.1,0.1), 0.5+random.uniform(-0.1,0.1)]
+            agent.state.p_vel = np.zeros(world.dim_p)
+            # agent.state.c = np.zeros(world.dim_c)
+        for i, landmark in enumerate(world.landmarks):
+            # landmark.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
+            if i < 19:
+                landmark.state.p_pos = np.array([i*(2/20)-1,0.8])
+            else:
+                landmark.state.p_pos = np.array([(i-20)*(2/20)-1,0.2])
+
+            landmark.state.p_vel = np.zeros(world.dim_p)
+
+    def reward(self, agent, world):
+        # dist2 = np.sum(np.square(agent.state.p_pos - world.landmarks[0].state.p_pos))
+        return 10
+
+    def observation(self, agent, world):
+        # get positions of all entities in this agent's reference frame
+        # entity_pos = []
+        # for entity in world.landmarks:
+        #     if not entity.boundary:
+        #         entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        # # communication of all other agents
+        # comm = []
+        # other_pos = []
+        # other_vel = []
+        # for other in world.agents:
+        #     if other is agent: continue
+        #     comm.append(other.state.c)
+        #     other_pos.append(list(np.array(other.state.p_pos) - np.array(agent.state.p_pos)))
+        #     # if not other.adversary:
+        #     other_vel.append(other.state.p_vel)
+        # return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos + other_vel)
+        return np.random.ranf((2+9*2*2))
